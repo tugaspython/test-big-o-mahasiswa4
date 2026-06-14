18a3b28ba138569edf59959dcd0fbd12e72f6023
mahasiswa4 <ican88030@gmail.com>
2026-06-14 19:55:58 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,1 +1,9 @@
+def cari_pasangan(arr, target):
+    terlihat = set()
+    for angka in arr:
+        komplemen = target - angka
+        if komplemen in terlihat:
+            return True
+        terlihat.add(angka)
+    return False
 
